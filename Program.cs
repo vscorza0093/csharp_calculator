e23553caf88cdeb17d50533030957627cc69b715
viniciusscorza <vinicius.scorza@hotmail.com>
2023-03-08 12:35:47 -0300

    Uploading project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float value1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(value1);
+        }
+    }
+}
