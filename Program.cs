2d7c384f77344e551a34c11059d40012303b8947
viniciusscorza <vinicius.scorza@hotmail.com>
2023-03-08 12:59:37 -0300

    Adding operations

--- Program.cs
+++ Program.cs
@@ -6,11 +6,60 @@ namespace Calculator
     {
         static void Main(string[] args)
         {   
-            Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
-            float value1 = float.Parse(Console.ReadLine());
+            Multiplication();
+        }
 
-            Console.WriteLine(value1);
+        static void Sum()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value:");
+            float firstValue = float.Parse(Console.ReadLine());
+            Console.WriteLine("Second value:");
+            float secondValue = float.Parse(Console.ReadLine());
+            float result = firstValue + secondValue;
+
+            Console.WriteLine($"Sum result: {result}");
+            Console.ReadKey();
+        }
+        static void Subtract()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value:");
+            float firstValue = float.Parse(Console.ReadLine());
+            Console.WriteLine("Second value:");
+            float secondValue = float.Parse(Console.ReadLine());
+            float result = firstValue - secondValue;
+
+            Console.WriteLine($"Subtraction result: {result}"); 
+            Console.ReadKey();
+        }
+        static void Division()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value:");
+            float firstValue = float.Parse(Console.ReadLine());
+            Console.WriteLine("Second Value:");
+            float secondValue = float.Parse(Console.ReadLine());
+            float result = firstValue / secondValue;
+
+            Console.WriteLine($"Division result: {result}");
+            Console.ReadKey();
+        }
+        static void Multiplication()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value:");
+            float firstValue = float.Parse(Console.ReadLine());
+            Console.WriteLine("Second value");
+            float secondValue = float.Parse(Console.ReadLine());
+            float result = firstValue * secondValue;
+
+            Console.WriteLine($"Multiplication result: {result}");
+            Console.ReadKey();
         }
     }
 }
